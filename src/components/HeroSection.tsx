ad103821188da29c715bea633c827a3ac216c1a4
Ashish kumar <ashishchumber34@gmail.com>
2024-06-09 20:03:00 +0530

    hero-section completed

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
+function HeroSection() {
+    return (
+        // Hero Section
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+            <Spotlight
+                className="-top-40 left-0 md:left-60 md:-top-20"
+                fill="white"
+            />
+            <div className='relative z-10 p-4 w-full text-center'>
+                <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the Art of Music</h1>
+                <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+                <div className="mt-4">
+                    <Link href="/courses">
+                    <Button
+                    borderRadius="1.75rem"
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                    Explore courses
+                    </Button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null)
+    return (
+        <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                <MenuItem setActive={setActive} active={active} item="Home">
+                    <div className="text-sm grid grid-cols-2 gap-10 p-4">
+                        <ProductItem
+                            title="Web Development"
+                            description="Learn how to build websites and web applications."
+                            href="/web-dev"
+                            src="/web-dev.svg"
+                        />
+                        <ProductItem
+                            title="Interface Design"
+                            description="Learn how to design user interfaces."
+                            href="/interface-design"
+                            src="/interface-design.svg"
+                        />
+                        <ProductItem
+                            title="Search Engine Optimization"
+                            description="Learn how to optimize websites for search engines."
+                            href="/seo"
+                            src="/seo.svg"
+                        />
+                        <ProductItem
+                            title="Branding"
+                            description="Learn how to create a brand identity."
+                            href="/branding"
+                            src="/branding.svg"
+                        />
+                    </div>
+                </MenuItem>
+                </Link>
+                <Link href={"/all-courses"}>
+                    <MenuItem setActive={setActive} active={active} item="Our courses">
+                        <div className="flex flex-col space-y-4 text-sm">
+                            <HoveredLink href="/all-courses">All courses</HoveredLink>
+                            <HoveredLink href="/music-theory">Basic Music Theory</HoveredLink>
+                            <HoveredLink href="/song-writting">songwritting</HoveredLink>
+                            <HoveredLink href="/music-production">Music Production</HoveredLink>
+                        </div>
+                    </MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Contact">
+                </MenuItem>
+            </Menu>
+        </div>
+    )
+}
+
+export default Navbar
